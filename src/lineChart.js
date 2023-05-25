4ebac0ab7fe8381ce5afaaeb8e75fd37f9b34e52
jjmmwon <jmw1790@gmail.com>
2023-05-25 23:43:35 +0900

    [UPDATE] linechart

--- src/lineChart.js
+++ src/lineChart.js
@@ -1,38 +1,71 @@
 import * as d3 from "d3";
 
 export function lineChart(data, attr) {
-  const margin = { top: 10, right: 10, bottom: 30, left: 30 };
+  const margin = { top: 30, right: 20, bottom: 30, left: 50 };
   const width = 400 - margin.left - margin.right;
-  const height = 150 - margin.top - margin.bottom;
+  const height = 180 - margin.top - margin.bottom;
 
   const years = [...new Set(data.map((d) => d.year))];
-  const means = years.map((y) =>
-    d3.mean(data.filter((d) => d.year === y).map((d) => d[attr]))
-  );
+  const means = years.map((y) => {
+    return {
+      year: y,
+      mean: d3.mean(data.filter((d) => d.year === y).map((d) => d[attr])),
+    };
+  });
 
-  const svg = d3
-    .create("svg")
+  const div = d3.create("div");
+  const svg = div
+    .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom);
+  const title = svg
+    .append("text")
+    .attr("x", 0)
+    .attr("y", 10)
+    .text(attr)
+    .attr("font-size", 12)
+    .attr("font-weight", "bold")
+    .attr("text-anchor", "start")
+    .attr("alignment-baseline", "hanging");
   const container = svg
     .append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
   const xAxis = svg
     .append("g")
-    .attr("transform", `translate(${margin.left}, ${height})`);
+    .attr("transform", `translate(${margin.left}, ${height + margin.top})`);
   const yAxis = svg
     .append("g")
-    .attr("transform", `translate(${margin.left}, 0)`);
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
   const xScale = d3.scaleLinear().domain(d3.extent(years)).range([0, width]);
   const yScale = d3
     .scaleLinear()
-    .domain(d3.extent(means).map((d) => d * 1.05))
+    .domain(
+      d3
+        .extent(means.map((d) => d.mean))
+        .map((d, i) => (i ? d * 1.03 : d * 0.97))
+    )
     .range([height, 0]);
+  console.log(means);
 
   xAxis.call(d3.axisBottom(xScale));
   yAxis.call(d3.axisLeft(yScale));
 
-  const lines = container.append("g");
+  let polyline = d3
+    .line()
+    .x((d) => xScale(d.year))
+    .y((d) => yScale(d.mean));
+
+  const line = container.append("g");
+  line
+    .append("path")
+    .attr("d", polyline(means))
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-width", 1);
+
+  return {
+    element: div.node(),
+  };
 }
--- src/observer.js
+++ src/observer.js
@@ -5,23 +5,40 @@ import { songsTable } from "./songs.js";
 import { lineChart } from "./lineChart.js";
 
 export function observer() {
-  let Scatterplot, ParallelCoordinates;
+  let Scatterplot, ParallelCoordinates, detailView;
   let sp, pc, st, lc;
+  let attrs = [
+    "duration_ms",
+    "danceability",
+    "tempo",
+    "energy",
+    // "popularity",
+    // "loudness",
+    // "acousticness",
+    // "speechiness",
+    // "instrumentalness",
+    // "liveness",
+    // "valence",
+  ];
 
   function initialize(data, DR) {
     console.log(data);
     console.log(DR);
     Scatterplot = d3.select(".SP");
     ParallelCoordinates = d3.select(".PC");
+    detailView = d3.select(".detailView");
 
     sp = scatterplot(DR);
     pc = parallelCoordinates(data);
     pc.update();
     st = songsTable(data);
-    lc = lineChart(data, "duration_ms");
+    lc = attrs.map((attr) => lineChart(data, attr));
 
     Scatterplot.node().appendChild(sp.element);
     ParallelCoordinates.node().appendChild(pc.element);
+    lc.forEach((l) => {
+      detailView.node().appendChild(l.element);
+    });
     return this;
   }
 
